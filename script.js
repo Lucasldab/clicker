28b3c37284ca17fe1a522adf39238eff8fdcb0d8
Lucas Barros <lucasldabarros@gmail.com>
2020-05-14 17:13:54 -0300

    + 1 Upgrade

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var bloodCount = 0;
-var autoClick = 0;
+var autoblood = 0;
+var bloodyflesh = 0;
 
 setInterval(timer, 1000);
 
@@ -10,12 +11,16 @@ function update() {
     } else {
         document.title = bloodCount + " Blood";
     }
-    document.getElementById("ammountMovingBlood").innerHTML = autoClick
-    document.getElementById("costMovingBlood").innerHTML = (autoClick + 1) * 12;
+    document.getElementById("ammountMovingBlood").innerHTML = autoblood
+    document.getElementById("costMovingBlood").innerHTML = (autoblood + 1) * 12;
+
+    document.getElementById("ammountBloodyFlesh").innerHTML = bloodyflesh
+    document.getElementById("costBloodyFlesh").innerHTML = (bloodyflesh + 1) * 15;
 }
 
 function timer() {
-    bloodCount = bloodCount + autoClick;
+    bloodCount = bloodCount + autoblood;
+    bloodCount = bloodCount + (bloodyflesh*2);
     update();
 }
 
@@ -26,21 +31,32 @@ function add() {
 
 function save() {
     localStorage.setItem("bloodCount", bloodCount);
-    localStorage.setItem("autoClick", autoClick);
+    localStorage.setItem("autoblood", autoblood);
+    localStorage.setItem("bloodyflesh", bloodyflesh);
 }
 
 function load() {
     bloodCount = localStorage.getItem("bloodCount");
     bloodCount = parseInt(bloodCount);
-    autoClick = localStorage.getItem("autoClick");
-    autoClick = parseInt(bloodCount);
+    autoblood = localStorage.getItem("autoblood");
+    autoblood = parseInt(autoblood);
+    bloodyflesh = localStorage.getItem("bloodyflesh");
+    bloodyflesh = parseInt(bloodyflesh);
     update();
 }
 
 function buyMovingBlood() {
-    if (bloodCount >= ((autoClick + 1) * 12)) {
-        bloodCount = bloodCount - ((autoClick + 1) * 12);
-        autoClick++;
+    if (bloodCount >= ((autoblood + 1) * 12)) {
+        bloodCount = bloodCount - ((autoblood + 1) * 12);
+        autoblood++;
+        update();
+    }
+}
+
+function buyBloodyFlesh() {
+    if (bloodCount >= ((bloodyflesh + 1) * 15)) {
+        bloodCount = bloodCount - ((bloodyflesh + 1) * 15);
+        bloodyflesh++;
         update();
     }
 }
