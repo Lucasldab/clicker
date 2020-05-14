ef4109285c20bcfdfb7808529725f7f2aa6f0a9e
Lucas Barros <lucasldabarros@gmail.com>
2020-05-13 22:52:16 -0300

    Adição de Salvar/Carregar e CSS

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 var bloodcount = 0
 
-function add(){
+function title() {
+    if (bloodcount > 1) {
+        document.title = bloodcount + " Bloods";
+    } else {
+        document.title = bloodcount + " Blood";
+    }
+}
+
+function add() {
     bloodcount++;
     document.getElementById('text').value = bloodcount;
-    document.title = bloodcount + " Blood";
+    title();
+}
+
+function save() {
+    localStorage.setItem("bloodcount", bloodcount);
+}
+
+function load() {
+    bloodcount = localStorage.getItem("bloodcount");
+    bloodcount = parseInt(bloodcount);
+    document.getElementById('text').value = bloodcount;
+    title();
 }
