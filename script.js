99dfc6d9b82c401e968579f24c2b71f679a1976c
Lucas Barros <lucasldabarros@gmail.com>
2020-05-16 14:53:52 -0300

    +3 Upgrades

--- script.js
+++ script.js
@@ -3,49 +3,61 @@ var autoblood = 0;
 var bloodyflesh = 0;
 var multiplier = 1;
 var redcells = 0;
+var bloodglyphs = 0;
+var bloodmana = 0;
+var bloodrunes = 0;
 
 setInterval(timer, 1000);
 
 function update() {
-    if (redcells>=1){
+    if (redcells >= 1) {
         document.getElementById('text').value = bloodCount.toFixed(1);
-    }else{
+    } else {
         document.getElementById('text').value = bloodCount
     }
     if (bloodCount > 1) {
-        if (redcells >= 1){
+        if (redcells >= 1) {
             document.title = bloodCount.toFixed(1) + " Bloods";
-        }else{
+        } else {
             document.title = bloodCount + " Bloods";
         }
     } else {
         document.title = bloodCount + " Blood";
     }
-    document.getElementById("ammountMultiplier").innerHTML = "Multiplier x" + (multiplier+1);
-    document.getElementById("ammountMultiplier2").innerHTML = "x" + (multiplier+1);
-    document.getElementById("costMultiplier").innerHTML = ((multiplier+1)*100) + " Bloods";
+    document.getElementById("ammountMultiplier").innerHTML = "Multiplier x" + (multiplier + 1);
+    document.getElementById("ammountMultiplier2").innerHTML = "x" + (multiplier + 1);
+    document.getElementById("costMultiplier").innerHTML = ((multiplier + 1) * 500) + " Bloods";
     document.getElementById("currentMultiplier").innerHTML = "Your current multiplier is x" + (multiplier);
 
     document.getElementById("ammountRedCells").innerHTML = redcells;
     document.getElementById("costRedCells").innerHTML = (redcells + 1) * 10;
 
     document.getElementById("ammountMovingBlood").innerHTML = autoblood;
-    document.getElementById("costMovingBlood").innerHTML = (autoblood + 1) * 12;
+    document.getElementById("costMovingBlood").innerHTML = (autoblood + 1) * 15;
 
     document.getElementById("ammountBloodyFlesh").innerHTML = bloodyflesh;
-    document.getElementById("costBloodyFlesh").innerHTML = (bloodyflesh + 1) * 15;
+    document.getElementById("costBloodyFlesh").innerHTML = (bloodyflesh + 1) * 25;
 
-    document.getElementById("bloodPerSecond").innerHTML = ((autoblood + (bloodyflesh*2))*multiplier) + " blood/s";
+    document.getElementById("ammountBloodGlyphs").innerHTML = bloodglyphs;
+    document.getElementById("costBloodGlyphs").innerHTML = (bloodglyphs + 1) * 50;
+
+    document.getElementById("ammountBloodMana").innerHTML = bloodmana;
+    document.getElementById("costBloodMana").innerHTML = (bloodmana + 1) * 100;
+
+    document.getElementById("ammountBloodRunes").innerHTML = bloodrunes;
+    document.getElementById("costBloodRunes").innerHTML = (bloodrunes + 1) * 200;
+
+    document.getElementById("bloodPerSecond").innerHTML = ((autoblood + (bloodyflesh * 2) + (bloodglyphs * 5) + (bloodmana * 10) + (bloodrunes * 15)) * multiplier) + " blood/s";
 }
 
 function timer() {
-    bloodCount = bloodCount + autoblood*multiplier;
-    bloodCount = bloodCount + (bloodyflesh*2)*multiplier;
+    bloodCount = bloodCount + autoblood * multiplier;
+    bloodCount = bloodCount + (bloodyflesh * 2) + (bloodglyphs * 5) + (bloodmana * 10) + (bloodrunes * 15) * multiplier;
     update();
 }
 
 function add() {
-    bloodCount = bloodCount + 1 + (redcells/10);
+    bloodCount = bloodCount + 1 + (redcells / 10);
     update();
 }
 
@@ -55,6 +67,9 @@ function save() {
     localStorage.setItem("bloodyflesh", bloodyflesh);
     localStorage.setItem("multiplier", multiplier);
     localStorage.setItem("redcells", redcells);
+    localStorage.setItem("bloodglyphs", bloodglyphs);
+    localStorage.setItem("bloodmana", bloodmana);
+    localStorage.setItem("bloodrunes", bloodrunes);
 }
 
 function load() {
@@ -68,10 +83,16 @@ function load() {
     multiplier = parseInt(multiplier);
     redcells = localStorage.getItem("redcells");
     redcells = parseInt(redcells);
+    bloodglyphs = localStorage.getItem("bloodglyphs");
+    bloodglyphs = parseInt(bloodglyphs);
+    bloodmana = localStorage.getItem("bloodmana");
+    bloodmana = parseInt(bloodmana);
+    bloodrunes = localStorage.getItem("bloodrunes");
+    bloodrunes = parseInt(bloodrunes);
     update();
 }
 
-function redCells() {
+function buyRedCells() {
     if (bloodCount >= ((redcells + 1) * 10)) {
         bloodCount = bloodCount - ((redcells + 1) * 10);
         redcells++;
@@ -80,25 +101,49 @@ function redCells() {
 }
 
 function buyMovingBlood() {
-    if (bloodCount >= ((autoblood + 1) * 12)) {
-        bloodCount = bloodCount - ((autoblood + 1) * 12);
+    if (bloodCount >= ((autoblood + 1) * 15)) {
+        bloodCount = bloodCount - ((autoblood + 1) * 15);
         autoblood++;
         update();
     }
 }
 
 function buyBloodyFlesh() {
-    if (bloodCount >= ((bloodyflesh + 1) * 15)) {
-        bloodCount = bloodCount - ((bloodyflesh + 1) * 15);
+    if (bloodCount >= ((bloodyflesh + 1) * 25)) {
+        bloodCount = bloodCount - ((bloodyflesh + 1) * 25);
         bloodyflesh++;
         update();
     }
 }
 
 function buyMultiplier() {
-    if (bloodCount >= ((multiplier+1)*100)){
-        bloodCount = bloodCount - ((multiplier+1)*100);
+    if (bloodCount >= ((multiplier + 1) * 500)) {
+        bloodCount = bloodCount - ((multiplier + 1) * 500);
         multiplier++;
+        update();
+    }
+}
+
+function buyBloodGlyphs() {
+    if (bloodCount >= ((bloodglyphs + 1) * 50)) {
+        bloodCount = bloodCount - ((bloodglyphs + 1) * 50);
+        bloodglyphs++;
+        update();
+    }
+}
+
+function buyBloodMana() {
+    if (bloodCount >= ((bloodmana + 1) * 100)) {
+        bloodCount = bloodCount - ((bloodmana + 1) * 100);
+        bloodmana++;
+        update();
+    }
+}
+
+function buyBloodRunes() {
+    if (bloodCount >= ((bloodrunes + 1) * 200)) {
+        bloodCount = bloodCount - ((bloodrunes + 1) * 200);
+        bloodrunes++;
         update();
     }
 }
